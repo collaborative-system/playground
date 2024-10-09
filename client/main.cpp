416f9f1c982456f3b1dfad3b4702fd03091aaa7e
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2024-10-09 23:17:49 +0200

    Add fuse init

--- client/main.cpp
+++ client/main.cpp
@@ -1,8 +1,11 @@
+#define FUSE_USE_VERSION 31
 #include "../common/io.h"
 #include "../common/log.h"
 #include "../proto/models.pb.h"
 #include <condition_variable>
 #include <cstdio>
+#include <fuse3/fuse.h>
+#include <fuse3/fuse_log.h>
 #include <google/protobuf/message.h>
 #include <map>
 #include <mutex>
@@ -51,9 +54,47 @@ int recv_thread(int sock) {
   return 0;
 }
 
-int main() {
+int sock;
+std::thread recv_th;
+
+static void *init(struct fuse_conn_info *conn, struct fuse_config *cfg) {
+  (void)conn;
+  cfg->kernel_cache = 1;
+  models::InitRequest init_request;
+  init_request.set_project_name("Project");
+  int err =
+      send_packet(sock, 1, models::PacketType::INIT_REQUEST, &init_request);
+  if (err < 0) {
+    log(ERROR, sock, "Error sending packet");
+    return NULL;
+  }
+  std::unique_lock<std::mutex> lock(mutexes[1]);
+  while (messages.find(1) == messages.end()) {
+    conditions[1].wait(lock);
+  }
+  models::InitResponse *init_response =
+      static_cast<models::InitResponse *>(messages[1]);
+  log(INFO, sock, "Received init response: %s", init_response->error());
+  return NULL;
+}
+
+static void destroy(void *private_data) {
+  (void)private_data;
+  close(sock);
+  recv_th.join();
+  google::protobuf::ShutdownProtobufLibrary();
+}
+
+static struct fuse_operations fuse_opr = {
+  .init = init,
+  .destroy = destroy,
+};
+
+int main(int argc, char *argv[]) {
+  int ret;
+  struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
   int port = 7400;
-  int sock = socket(AF_INET, SOCK_STREAM, 0);
+  sock = socket(AF_INET, SOCK_STREAM, 0);
   if (sock < 0) {
     log(ERROR, sock, "Error creating socket: %s", strerror(errno));
     return 1;
@@ -68,28 +109,12 @@ int main() {
   }
   log(INFO, sock, "Connected to port %d", port);
 
-  std::thread recv(recv_thread, sock);
-  models::InitRequest init_request;
-  init_request.set_project_name("Project");
-  int err =
-      send_packet(sock, 1, models::PacketType::INIT_REQUEST, &init_request);
-  if (err < 0) {
-    log(ERROR, sock, "Error sending packet");
-    return 1;
-  }
-  std::unique_lock<std::mutex> lock(mutexes[1]);
-  while (messages.find(1) == messages.end()) {
-    conditions[1].wait(lock);
-  }
-  models::InitResponse *init_response =
-      static_cast<models::InitResponse *>(messages[1]);
-  (void)init_response;
-  lock.unlock();
-  messages.erase(1);
-  conditions.erase(1);
-  mutexes.erase(1);
-  recv.detach();
+  std::thread tmp(recv_thread, sock);
+  recv_th.swap(tmp);
+  ret = fuse_main(args.argc, args.argv, &fuse_opr, NULL);
+  fuse_opt_free_args(&args);
+  recv_th.detach();
   close(sock);
   google::protobuf::ShutdownProtobufLibrary();
-  return 0;
+  return ret;
 }
